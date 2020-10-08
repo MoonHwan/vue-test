e557b3276a89a76f0b1cd17b050952988b14f511
권현지/모바일개발팀 <khjbest39@golfzon.com>
2020-10-08 17:57:42 +0900

    ex project

---
+++ FirstTestProject/simple-board/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="app">
+    <v-app-bar>
+      Simple Board
+    </v-app-bar>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name:'App'
+};
+</script>
---
+++ FirstTestProject/simple-board/src/router/index.js
@@ -0,0 +1,27 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import BoardList from '@/components/BoardList'
+import BoardView from '@/components/BoardView'
+import BoardWriter from '@/components/BoardWriter'
+
+Vue.use(Router)
+
+export default new Router({
+    routes:[
+        {
+            path:'/',
+            name:'BoardList',
+            component:BoardList
+        },
+        {
+            path:'/',
+            name:'BoardList',
+            component:BoardView
+        },
+        {
+            path:'/writer',
+            name:'BoardWriter',
+            component:BoardWriter
+        }
+    ]
+})
