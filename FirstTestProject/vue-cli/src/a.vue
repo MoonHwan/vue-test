06bf2717cf4728ad5a15f3e70e11c27892e914f8
권현지/모바일개발팀 <khjbest39@golfzon.com>
2020-10-12 16:13:34 +0900

    vue프로젝트 실습

---
+++ FirstTestProject/vue-cli/src/a.vue
@@ -0,0 +1,13 @@
+<template>
+<div>
+</div>
+<!-- 싱글파일 컴포넌트     -->
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style>
+
+</style>
---
+++ FirstTestProject/vue-cli/src/main.js
@@ -0,0 +1,11 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+
+createApp(App).mount('#app')
+
+// new Vue({
+//     render:h=>h(App),
+//     components:{
+
+//     }
+// }).$mount('#app')
