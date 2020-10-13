c2b23c73d92f30a56df25f1a50478d8b0036d95b
권현지/모바일개발팀 <khjbest39@golfzon.com>
2020-10-13 17:55:47 +0900

    할일 앱 수정(6-4까지)

--- vue-json/src/components/TodoFooter.vue
+++ vue-json/src/components/TodoFooter.vue
@@ -1,8 +1,28 @@
 <template>
-    <div>footer</div>
+    <div class="clearALlContainer">
+        <span class="clearAllBtn" @click="clearTodo">모두 다 지워</span>    
+    </div>
 </template>
 <script>
 export default {
-    
+    methods:{
+        clearTodo(){
+            localStorage.clear();
+        }
+    }
 }
 </script>
+<style scoped>
+    .clearALlContainer{
+        width: 8.5rem;
+        height: 50px;
+        line-height: 50px;
+        background-color: darkseagreen;
+        border-radius: 5px;
+        margin: 0 auto;
+    }
+    .clearAllBtn{
+        color:blue;
+        display: black;
+    }
+</style>
